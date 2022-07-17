6fb5f0aedcb185200545e3fb881fecffea981652
adarsh jambhulkar <ajambhulkar27@gmail.com>
2022-07-17 18:06:39 +0530

    fix project layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,6 @@
 'use strict';
 
 
-// https://colorhunt.co/palette/1b262c0f4c753282b8bbe1fa
-// https://colorhunt.co/palette/14285027496d0c7b9300a8cc
-// https://colorhunt.co/palette/1a1a2e16213e0f3460e94560
-// https://colorhunt.co/palette/1a1a2e16213e0f3460e94560
-
-
-
-
-
-
-
-
-
-
-
 $(".btn").click(function(){
   $(".adarsh").toggleClass(" active");
 });
